b2ab17e1180c48b74465f020c8cf1f22b28be4e3
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-07-26 22:58:15 -0700

    basic styling for contact

--- personal-portfolio/src/components/Contact.js
+++ personal-portfolio/src/components/Contact.js
@@ -4,7 +4,12 @@ const Contact = () => {
 
     return (
         <div className="watercolour-container">
-            
+            <h2>Contact Me</h2>
+            <div className="link-icons">
+                <a href="" >LinkedIn</a>
+                <a href="" >GitHub</a>
+                <a href="" >Email</a>
+            </div>
         </div>
     )
 }
