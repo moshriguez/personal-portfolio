e6a5f481c1fe86956d37f49f4cb39aa1b6614de0
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-07-28 00:30:52 -0700

    added motion to home and nav

--- personal-portfolio/src/components/Home.js
+++ personal-portfolio/src/components/Home.js
@@ -1,12 +1,24 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 const Home = () => {
+    const risingSun = {
+        start: {
+            opacity: 1,
+            y: 500
+        },
+        end: {
+            y: 0,
+            opacity: 1, 
+            transition: {duration: 3, delay: 2}
+        }
+    }
 
     return (
-        <div className="home">
+        <motion.div className="home" variants={risingSun} initial="start" animate="end">
             <h1>Marc</h1>
             <h1>Rodriguez</h1>
-        </div>
+        </motion.div>
     )
 }
 
--- personal-portfolio/src/components/navigation.js
+++ personal-portfolio/src/components/navigation.js
@@ -1,16 +1,41 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { motion } from "framer-motion";
+
 
 const Navigation = () => {
+    const risingSun = {
+        start: {
+            opacity: 0
+        },
+        end: {
+            opacity: 1, 
+            transition: {
+                staggerChildren: 0.5,
+                delayChildren: 4
+            }
+        }
+    }
+    const variant = {
+        start: {
+            y: 100
+        }, 
+        end: {
+            y: 0,
+            transition: {
+                duration: 2
+            }        
+        }
+    }
 
     return (
-        <ul className="menu">
-            <li><NavLink to="/">home</NavLink></li>
-            <li><NavLink to="/about">about</NavLink></li>
-            <li><NavLink to="/projects">projects</NavLink></li>
-            <li><NavLink to="/blogs">blogs</NavLink></li>
-            <li><NavLink to="/contact">contact</NavLink></li>
-        </ul>
+        <motion.ul className="menu" variants={risingSun} initial="start" animate="end">
+            <motion.li variants={variant}><NavLink to="/">home</NavLink></motion.li>
+            <motion.li variants={variant}><NavLink to="/about">about</NavLink></motion.li>
+            <motion.li variants={variant}><NavLink to="/projects">projects</NavLink></motion.li>
+            <motion.li variants={variant}><NavLink to="/blogs">blogs</NavLink></motion.li>
+            <motion.li variants={variant}><NavLink to="/contact">contact</NavLink></motion.li>
+        </motion.ul>
       
       )
   
