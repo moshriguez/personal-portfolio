141e9df87bb5d3fcae3dba7ad2af802d37044be9
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-07-29 23:53:41 -0700

    completed blogs helper component

---
+++ personal-portfolio/src/helpers/blogs.js
@@ -0,0 +1,37 @@
+export const blogs = [
+    {
+        name: 'Using the Web Audio API: Where Programming meets Sound Engineering',
+        brief: 'Before I discovered my love of programming, I went to school for music recording and learned a lot about audio processing. When I first…',
+        time: '6',
+        image: 'https://miro.medium.com/max/1400/1*AEbQcQ9fDMxFty01r-4sxQ.jpeg',
+        tags: ['Javascript', 'Web Audio API', 'Programming']
+    },
+    {
+        name: 'It’s About Time: Representing and Manipulating Time in Ruby',
+        brief: 'As humans, we use time and dates often, so it makes sense that we would want a structure to keep track of them in our code. Ruby has three…',
+        time: '4',
+        image: 'https://miro.medium.com/max/1400/1*Xem1rbG0Fa8DAhtez7mvsA.jpeg',
+        tags: ['Ruby', 'Time', 'Date', 'DateTime']
+    },
+    {
+        name: 'Regular Expressions',
+        brief: 'Regular expressions (often referred to as Regex) are used in many programming languages to match patterns of characters in strings. Their…',
+        time: '6',
+        image: 'https://miro.medium.com/max/1400/1*x4cvN7eIKV7UiPXdYldhVg.jpeg',
+        tags: ['Regular Expressions', 'Javascript', 'Programming']
+    },
+    {
+        name: 'Controlled Forms in React',
+        brief: 'When I first encountered controlled forms in React, I was a bit confused. I had a hard time following exactly what was going on. But then…',
+        time: '4',
+        image: 'https://miro.medium.com/max/1000/1*Z43Gzoxhuc49z89EU6dPlA.jpeg',
+        tags: ['Reactjs', 'Webdev'] 
+    },
+    {
+        name: 'Recursion and the Call Stack: An Explanation of Recursive Functions for Beginners',
+        brief: 'Recursion can be a difficult concept to understand, especially as a beginner. It certainly boggled my mind the first time I learned about…',
+        time: '7',
+        image: 'https://miro.medium.com/max/1400/1*q9jYBOS9Q__w6DahcpFroQ.jpeg',
+        tags: ['JavaScript', 'Recursion', 'Call Stack']
+    }
+]
