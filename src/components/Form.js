b4300f61c4ab9d96d649ecbaa7f9dc7daf77c6d5
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-09-20 23:56:30 -0700

    form will not submit if empty; and styling

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,9 +2,46 @@ import React, {useState} from "react";
 
 const Form = () => {
     const [contactForm, setContactForm] = useState({name: '', message: '', email: '', subject: ''})
-    
+    const [errors, setErrors] = useState([])
+
+    const handleChange = (e) => {
+        setContactForm({
+            ...contactForm,
+            [e.target.name]: e.target.value
+        })
+    }
+
     const handleSubmit = (e) => {
         e.preventDefault()
+        validateForm()
+    }
+
+    const validateForm = () => {
+        const {name, message, email, subject} = contactForm
+        const newErrors = []
+        if (!name) {
+            newErrors.push('Please enter your name.')
+        }
+        if (!message) {
+            newErrors.push('Please enter a message.')
+        }
+        if (!email) {
+            newErrors.push('Please enter your email.')
+        }
+        if (!subject) {
+            newErrors.push('Please enter a subject.')
+        }
+        if (newErrors.length) {
+            console.log(newErrors)
+            setErrors(newErrors)
+        } else {
+            handleFetch()
+            console.log('no errors')
+        }
+        return !errors.length
+    }
+
+    const handleFetch = () => {
         const {name, message, email, subject} = contactForm
         const emailContent = {
             from_name: name,
@@ -22,22 +59,16 @@ const Form = () => {
         }
         fetch('https://radiant-island-38686.herokuapp.com/', config)
         .then(res => res.json())
-        .then(console.log)
-        resetForm()
+        .then((res) => {
+            console.log(res)
+            resetForm()
+        })
+        .catch(err => setErrors([...errors, err]))
     }
 
     const resetForm = () => {
         setContactForm({name: '', message: '', email: '', subject: ''})
     }
-
-    const handleChange = (e) => {
-        setContactForm({
-            ...contactForm,
-            [e.target.name]: e.target.value
-        })
-    }
-
-
 
     return (
         <form onSubmit={handleSubmit} >
@@ -48,7 +79,7 @@ const Form = () => {
             <label htmlFor="subject">Subject</label>
             <input type="text" name="subject" onChange={handleChange} value={contactForm.subject} />
             <label htmlFor="message">Message</label>
-            <input type="textarea" name="message" onChange={handleChange} value={contactForm.message} />
+            <textarea type="textarea" name="message" onChange={handleChange} value={contactForm.message} />
             <input type="submit" value="submit" />
         </form>
     )
