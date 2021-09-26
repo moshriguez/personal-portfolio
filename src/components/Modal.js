f4f772a5a150f22a7c898b8ced4df3997ef94f21
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-09-26 13:18:18 -0700

    more work on the modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -2,10 +2,33 @@ import React from "react";
 
 const Modal = ({ message, resetForm, setSending, errors }) => {
 
+    const renderErrors = () => {
+        errors.map((error, i) => {
+            return <li key={i} >{error}</li>
+        })
+    }
+
+    const ok = () => {
+        resetForm()
+        setSending(false)
+    }
+
     return (
         <div className="modal-background">
             <div className="modal-content">
-                <p>{message}</p>
+                {errors.length > 0 ? (
+                    <>
+                        <h3>Errors:</h3>
+                        <div>
+                            {renderErrors()}
+                        </div>
+                    </>
+                ) : (
+                    <>
+                        <p>{message}</p>
+                        {/successful/.test(message) ? <button onClick={ok} >OK</button> : null}
+                    </>
+                )}
             </div>
         </div>
     )
